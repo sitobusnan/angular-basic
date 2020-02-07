6ce1dc455f2da75ca2edec4172796146ff5ba24b
sitobusnan <sito@citytonight.es>
2020-02-07 20:17:17 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-basic';
+  name = 'Sito';
+  ocupation = 'Buscando la solución';
 }
