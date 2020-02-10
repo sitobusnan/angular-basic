e25af92bf02b1247ee949aed8b545a713689e61d
sitobusnan <sito@citytonight.es>
2020-02-10 16:56:42 +0100

    done

--- src/app/Components/Header/header.component.ts
+++ src/app/Components/Header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `<h1>Header Component</h1>`
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
     menu: 'Opciones';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/Header/header.component';
-
+import { BodyComponent } from './Components/Body/body.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
